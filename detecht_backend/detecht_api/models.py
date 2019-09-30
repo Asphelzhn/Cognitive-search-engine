8d89b4fbd654682422976e9b502e2c91b661de00
Oskar <oskhi827@student.liu.se>
2019-09-30 11:56:27 +0200

    Database setup, User
    makemigrations & migrate

--- detecht_backend/detecht_api/models.py
+++ detecht_backend/detecht_api/models.py
@@ -1,25 +1,12 @@
 from django.db import models
-from django.contrib.auth.models import (AbstractBaseUser)
 
 
 # Create your models here.
 
-# BEGIN: Code written by Armin
-class User(AbstractBaseUser):
-    email = models.EmailField(max_length=255, unique=True, verbose_name="email address")
-    full_name = models.CharField(max_length=255, blank=True, null=True)
-    admin = models.BooleanField(default=False)  # True if user is admin
-    # password
-    # age
+#By OskarH
+class User(models.Model):
+    userName = models.TextField(max_length=50)
+    firstName = models.TextField(max_length=50)
 
+#end OskarH
 
-    def __str__(self):
-        return self.email
-
-    def get_full_name(self):
-        return self.full_name
-
-    @property
-    def is_admin(self):
-        return self.admin
-# END: Code written by Armin
