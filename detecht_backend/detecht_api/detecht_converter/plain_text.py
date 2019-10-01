6e777157f780c5ed5336c55c5f474bffb0958fa9
Oscar Wester <oscwe565@student.liu.se>
2019-10-01 17:06:52 +0200

    Can collect attribute from Json object and return as string

--- detecht_backend/detecht_api/detecht_converter/plain_text.py
+++ detecht_backend/detecht_api/detecht_converter/plain_text.py
@@ -1,3 +1,16 @@
+import json
+
 
 def test_message(message):
     return "Hello, message: " + message
+
+
+def json_to_plaintext(jsonfile, attribute):
+
+    read = jsonfile.read()
+    json_as_string = json.loads(read)
+    r = json_as_string[attribute]
+
+
+
+    return r
