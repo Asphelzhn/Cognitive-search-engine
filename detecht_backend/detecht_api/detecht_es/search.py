b1a8b03e28ded78796210e275d5f1b7cacb09554
Jakob <nilssonlindau@gmail.com>
2019-10-03 13:25:41 +0200

    Change structure and removed unused functions

--- detecht_backend/detecht_api/detecht_es/search.py
+++ detecht_backend/detecht_api/detecht_es/search.py
@@ -11,20 +11,6 @@ else:
     print('Could not connect to elasticsearch')
 
 
-def single_search(query):
-    body = {
-        "fields": ["title"],
-        "size": 1,
-        "query": {
-            "query_string": {
-                "fields": ["file.content", "file.file.name"],
-                "query": query
-            }
-        }
-    }
-    return es.search(index="db", doc_type="doc", body=body)
-
-
 # Should handle both single and multiple searches.
 def search(query, size=1):
     body = {
@@ -41,27 +27,21 @@ def search(query, size=1):
     return res
 
 
-def format_search(result):
+def formated_search(query, size=1):
+    body = {
+        "_source": ["title"],
+        "size": size,
+        "query": {
+            "query_string": {
+                "query": query
+            }
+        }
+    }
+
+    res = es.search(index="db", body=body)
     titles = list()
     for hit in res['hits']['hits']:
         title = "%(title)s" % hit["_source"]
         titles.append(title)
     return titles
 
-
-search("Rupert")
-
-
-def formated_search(query, size=1):
-    body = {
-        "_source": ["file.file.filename"],
-        "query": {
-            "query_string": {
-                "fields": ["file.content", "file.file.name"],
-                "query": query,
-                "size": size
-            }
-        }
-    }
-
-    return es.search(index="db", doc_type="doc", body=body)
