73dbda0d53237ee49270cf1c8adc59ccc06dc431
Jakob Lindau <nilssonlindau@gmail.com>
2019-10-08 11:45:16 +0200

    Basic getters and setters for json class

---
+++ detecht_backend/detecht_api/detecht_es/json_class.py
@@ -0,0 +1,66 @@
+import json
+
+
+String pdf_name
+tags = list()
+sections = list()
+keywords = list()
+String full_text
+
+
+def insert_json(json):
+
+
+def export_json():
+
+    return json
+
+# Adds whole document to one string, can be a problem with memory management
+def add_full_text(fulltext):
+    full_text = fulltext
+
+
+def get_full_text():
+    return full_text
+
+
+def add_tag(tag):
+    if tag not in tags
+        tags.append(tag)
+
+
+def remove_tag(tag):
+    if tag in tags
+        tags.remove(tag)
+
+
+def get_tags():
+    return tags
+
+
+def add_keyword(keyword):
+    if keyword not in keywords
+        keywords.append(keyword)
+
+
+def remove_keyword(keyword):
+    if keyword in keywords
+        keywords.remove(keyword)
+
+
+def get_keywords():
+    return keywords
+
+
+def add_pdf_name(pdfname):
+    pdf_name = pdfname
+
+
+def get_pdf_name():
+    return pdf_name
+
+
+def add_section(start, end, section_tags):
+
+
+def get_sections():
---
+++ detecht_backend/detecht_api/detecht_es/search.py
@@ -0,0 +1,48 @@
+import json, requests, os
+from elasticsearch import Elasticsearch
+
+# res = requests.get('http://localhost:9200')
+# print(res.content)
+es = None
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+if es.ping():
+    print('Connected to elasticsearch')
+else:
+    print('Could not connect to elasticsearch')
+
+
+# Should handle both single and multiple searches.
+def search(query, size=1):
+    body = {
+        #"_source": ["title"],
+        "size": size,
+        "query": {
+            "query_string": {
+                "query": query
+            }
+        }
+    }
+    es.indices.refresh(index="db")
+    res = es.search(index="db", body=body)
+    return res
+
+
+def formated_search(query, size=1):
+    body = {
+        "_source": ["title"],
+        "size": size,
+        "query": {
+            "query_string": {
+                "query": query
+            }
+        }
+    }
+
+    res = es.search(index="db", body=body)
+    titles = list()
+    for hit in res['hits']['hits']:
+        title = "%(title)s" % hit["_source"]
+        titles.append(title)
+    return titles
+
+
