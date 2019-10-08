7b2a0ce46346422ba3574310a1c6fea151861c25
Jakob <nilssonlindau@gmail.com>
2019-10-08 12:23:01 +0200

    Changed type to class and added testfile

--- detecht_backend/detecht_api/detecht_converter/json_class.py
+++ detecht_backend/detecht_api/detecht_converter/json_class.py
@@ -1,66 +1,70 @@
 import json
 
 
-String pdf_name
-tags = list()
-sections = list()
-keywords = list()
-String full_text
+class json_class:
+
+    def __init__(self):
+        pdf_name = ""
+        tags = list()
+        sections = list()
+        keywords = list()
+        full_text = ""
+    #def insert_json(json):
 
 
-def insert_json(json):
+    #def export_json():
+
+     #   return json
+
+    # Adds whole document to one string, can be a problem with memory management
+    def add_full_text(self, fulltext):
+        self.full_text = fulltext
 
 
-def export_json():
-
-    return json
-
-# Adds whole document to one string, can be a problem with memory management
-def add_full_text(fulltext):
-    full_text = fulltext
+    def get_full_text(self):
+        return self.full_text
 
 
-def get_full_text():
-    return full_text
+    def add_tag(tag):
+        if tag not in tags:
+            tags.append(tag)
 
 
-def add_tag(tag):
-    if tag not in tags
-        tags.append(tag)
+    def remove_tag(tag):
+        if tag in tags:
+            tags.remove(tag)
 
 
-def remove_tag(tag):
-    if tag in tags
-        tags.remove(tag)
+    def get_tags():
+        return tags
 
 
-def get_tags():
-    return tags
+    def add_keyword(keyword):
+        if keyword not in keywords:
+            keywords.append(keyword)
 
 
-def add_keyword(keyword):
-    if keyword not in keywords
-        keywords.append(keyword)
+    def remove_keyword(keyword):
+        if keyword in keywords:
+            keywords.remove(keyword)
 
 
-def remove_keyword(keyword):
-    if keyword in keywords
-        keywords.remove(keyword)
+    def get_keywords():
+        return keywords
 
 
-def get_keywords():
-    return keywords
+    def add_pdf_name(self,pdfname):
+        self.pdf_name = pdfname
 
 
-def add_pdf_name(pdfname):
-    pdf_name = pdfname
+    def get_pdf_name(self):
+        print("PdfName")
+        return self.pdf_name
 
 
-def get_pdf_name():
-    return pdf_name
+    #def add_section(start, end, section_tags):
 
 
-def add_section(start, end, section_tags):
+    #def get_sections():
 
 
-def get_sections():
---
+++ detecht_backend/detecht_api/detecht_converter/json_class_testfile.py
@@ -0,0 +1,5 @@
+from json_class import *
+
+json = json_class()
+json.add_pdf_name("pdf.pdf")
+print(json.get_pdf_name())
