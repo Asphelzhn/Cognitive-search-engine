0f5c728e50e9a229981861fff168e3d02144f0bd
Nicklas Kull <nicku318@student.liu.se>
2019-10-08 09:44:42 +0200

    Fixing styling so the pipeline will work!

--- detecht_backend/detecht_backend/settings.py
+++ detecht_backend/detecht_backend/settings.py
@@ -78,8 +78,7 @@ ROOT_URLCONF = 'detecht_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
