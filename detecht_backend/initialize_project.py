f8ff1c1459c2c37c201e0e41fed6e2c90a26f70d
Nicklas Kull <nicku318@student.liu.se>
2019-10-08 11:38:18 +0200

    fixed typo in init file

--- detecht_backend/initialize_project.py
+++ detecht_backend/initialize_project.py
@@ -2,5 +2,5 @@ from elasticsearch import Elasticsearch
 es = Elasticsearch()
 
 # ignore 400 cause by IndexAlreadyExistsException when creating an index
-es.indices.create(index='test-index', ignore=400)
+es.indices.create(index='db', ignore=400)
 
