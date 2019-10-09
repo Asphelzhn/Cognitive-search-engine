12e69eb7ec3e52fab3e2b26189aeda053ff33cf3
asphelzhn <568718851@qq.com>
2019-10-09 21:31:10 +0200

    change tfidf api

--- detecht_backend/detecht_api/detecht_nlp/keywordExtraction/rake_api.py
+++ detecht_backend/detecht_api/detecht_nlp/keywordExtraction/rake_api.py
@@ -18,14 +18,16 @@ class Rake4Keyword():
         :return: (key word, weight) for most relevant keywords
         """
         stoppath = 'rake_algorithm/data/stoplists/SmartStoplist.txt'
-        rake_object = rake.Rake(stoppath, 5, 3, 2)
+        rake_object = rake.Rake(stoppath, 5, 3, 4)
         keywords = rake_object.run(text)
+        return keywords
 
 
 '''example using RAKE to extract keyword
 '''
 
 if __name__ == '__main__':
+
     text = """Python is an interpreted, high-level, general-purpose programming language. Created by Guido van Rossum and first released in 1991, Python's design philosophy emphasizes code readability with its notable use of significant whitespace. Its language constructs and object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects.[28]
 
 Python is dynamically typed and garbage-collected. It supports multiple programming paradigms, including procedural, object-oriented, and functional programming. Python is often described as a "batteries included" language due to its comprehensive standard library.[29]
