0a927388fa1e25f0dc9e23b18fe7b6d57d4b0038
Henrik Hörnlund <henho110@student.liu.se>
2019-10-09 10:48:24 +0200

    The sentence class , for creating the array of important sentences based on query for each document

---
+++ detecht_backend/detecht_api/detecht_nlp/class_imp_set.py
@@ -0,0 +1,32 @@
+
+"""
+Henrik & Oscar
+"""
+
+
+class ImpSent:
+    sent = str
+    rank = int
+    order = int
+    start_index = int
+    end_index = int
+
+
+def set_sent(sent_input):
+    sent = sent_input
+
+
+def set_rank(sent_rank):
+    rank = sent_rank
+
+
+def set_order(sent_order):
+    order = sent_order
+
+
+def set_start_index(sent_start):
+    start_index = sent_start
+
+
+def set_end_index(sent_end):
+    end_index = sent_end
