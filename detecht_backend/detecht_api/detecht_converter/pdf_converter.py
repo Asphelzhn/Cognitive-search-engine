5ef56710e0aaf23254223c9615d4ed8972d6bb9e
Carl Björklund <carlbjo@gmail.com>
2019-10-15 09:08:51 +0200

    Started modifying converter towards only plain text ingestion

--- detecht_backend/detecht_api/detecht_converter/pdf_converter.py
+++ detecht_backend/detecht_api/detecht_converter/pdf_converter.py
@@ -1,27 +1,20 @@
 import PyPDF2
 import json
-from os import listdir
-from os.path import isfile, join
+import re
 
 # get the PDF path and read the file
-file = "BOAML_report_2017.pdf"
+file = "../static/pdf/BOAML_report_2016.pdf"
 read_pdf = PyPDF2.PdfFileReader(file, strict=False)
-
-# get the read object's meta info
-pdf_meta = read_pdf.getDocumentInfo()
 all_pages = {}
-all_pages["meta"] = {}
 
-for meta, value in pdf_meta.items():
-    all_pages["meta"][meta] = value
 
 # iterate the page numbers
 for page in range(read_pdf.getNumPages()):
     data = read_pdf.getPage(page)
-    page_mode = read_pdf.getPageMode()
+    #page_mode = read_pdf.getPageMode()
     page_text = data.extractText()
-    all_pages[page] = page_text
-
+    modified_text = re.sub("\n","",page_text)
+    all_pages[page] = modified_text
 
 # create a JSON string from the dictionary
 json_data = json.dumps(all_pages)
