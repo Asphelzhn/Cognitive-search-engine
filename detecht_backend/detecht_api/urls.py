8664ea145657aa7c0bd29a02c1914edb3377c94a
Oskar <oskhi827@student.liu.se>
2019-10-15 10:37:49 +0200

    test update

--- detecht_backend/detecht_api/urls.py
+++ detecht_backend/detecht_api/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     url(r'^api/profile/$', views.Profile.as_view()),
     url(r'^api/updateprofile/$', views.UpdateProfile.as_view()),
     url(r'^api/keyword/$', views.Keyword.as_view()),
+    url(r'^api/keywordsimilarity/$', views.KeywordSimilarity.as_view()),
     url(r'^api/', include(router.urls)), #files
 ]
--- detecht_backend/detecht_api/views.py
+++ detecht_backend/detecht_api/views.py
@@ -122,8 +122,6 @@ class Keyword(APIView):
         wordToStore = input["keyword"]
         message = Keywords.add_keyword(wordToStore)
 
-        message = Keyword_distance.add_keyword_distance(id1=Keywords.objects.get(word="banan").id, id2=Keywords.objects.get(word="apa").id, similarity=1.0000)
-        #message = Keyword_distance.objects.get(id=1)
         return HttpResponse(message)
 
 class KeywordSimilarity(APIView):
@@ -131,9 +129,8 @@ class KeywordSimilarity(APIView):
     def post(self, request): #input: keyword1, keyword2, similarity
         input = request.data
 
-        message = Keyword_distance.add_keyword_distance(id1=Keywords.objects.get(word=input["keyword1"]).id,
-                                                    id2=Keywords.objects.get(word=input["keyword2"]).id, similarity=input["similarity"])
-        #message = Keyword_distance.objects.get(id=1)
+        message = Keyword_distance.add_keyword_distance(id1=Keywords.objects.get(word=input["keyword1"]).id, id2=Keywords.objects.get(word=input["keyword2"]).id, similarity=input["similarity"])
+        # message = Keyword_distance.objects.get(id=1)
         return HttpResponse(message)
 
 # END: Code written by Armin
