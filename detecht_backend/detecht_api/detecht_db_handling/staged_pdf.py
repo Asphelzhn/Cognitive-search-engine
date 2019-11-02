1ed21f1e2046f4c3026164620013accf065874c7
Oskar <oskhi827@student.liu.se>
2019-11-02 13:55:50 +0100

    cannot import StagedPdf

---
+++ detecht_backend/detecht_api/detecht_db_handling/staged_pdf.py
@@ -0,0 +1,30 @@
+from detecht_api.models import StagedPdf
+from detecht_api.models import StagedPdfTags
+
+
+def add_staged_pdf(pdf_name, title):
+    new_pdf = StagedPdf(pdf_name=pdf_name, title=title)
+    new_pdf.save()
+    return
+
+
+def remove_staged_pdf(title):
+    try:
+        StagedPdf.objects.filter(title=title).delete()
+    except:
+        return False #returns false if no object is deleted.
+    return True #returns true if object is removed successfully.
+
+
+def add_staged_pdf_tag(staged_pdf_id, tag):
+    new_tag = StagedPdfTags(staged_pdf_id=staged_pdf_id, tag=tag)
+    new_tag.save()
+    return
+
+
+def remove_staged_pdf_tag(staged_pdf_id, tag):
+    try:
+        StagedPdfTags.objects.filter(staged_pdf_id=staged_pdf_id, tag=tag)
+    except:
+        return False #returns false if no object is deleted
+    return True #returns true if object is removed successfully
