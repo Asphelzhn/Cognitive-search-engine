ed415adfe67ff86321c8e743c94d6a03a40fdd94
Oskar <oskhi827@student.liu.se>
2019-11-04 14:55:51 +0100

    function add staged pdfs to Es added in db_handling

--- detecht_backend/detecht_api/detecht_db_handling/staged_pdf.py
+++ detecht_backend/detecht_api/detecht_db_handling/staged_pdf.py
@@ -1,6 +1,6 @@
 from detecht_api.models import StagedPdf
 from detecht_api.models import StagedPdfTags
-
+from detecht_api.detecht_es import insert_file
 
 
 def add_staged_pdf(pdf_name, title):
@@ -29,3 +29,12 @@ def remove_staged_pdf_tag(staged_pdf_id, tag):
     except:
         return False #returns false if no object is deleted
     return True #returns true if object is removed successfully
+
+def insert_all_staged_pdf_into_es():
+    all_pdfs = StagedPdf.objects.all()
+
+    for a in all_pdfs:
+        json_string = '{"title":"' + a.title + '", "fileName":"' + a.pdf_name + '"}'
+        insert_file.inject_one_file(json_string)
+        remove_staged_pdf(a.title)
+    return
