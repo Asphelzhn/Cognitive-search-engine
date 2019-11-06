9a8222d1ebeabe382483e25e0e0629997ec7478e
Sara Nilsson <sarni637@student.liu.se>
2019-11-06 18:51:36 +0100

    added router to start page, router start

--- detecht-frontend/src/app/app-routing.module.ts
+++ detecht-frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 import {SearchPageComponent} from './search-page/search-page.component';
 import {UploadFileComponent} from './admin-page/upload-file/upload-file.component';
+import {StartPageComponent} from "./start-page/start-page.component";
 
 
 const routes: Routes = [
   { path: '', component: SearchPageComponent },
   { path: 'admin', component: AdminPageComponent },
-  { path: 'upload', component: UploadFileComponent }
+  { path: 'upload', component: UploadFileComponent },
+  { path: 'start', component: StartPageComponent }
 ];
 
 @NgModule({
