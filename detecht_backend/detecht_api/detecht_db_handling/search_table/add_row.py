04b8cbc45af2906ee117341478a113d0d5bb122c
asphelzhn <568718851@qq.com>
2019-11-07 15:31:21 +0100

    add model

--- detecht_backend/detecht_api/detecht_db_handling/search_table/add_row.py
+++ detecht_backend/detecht_api/detecht_db_handling/search_table/add_row.py
@@ -1,20 +1,20 @@
 # from detecht_api import models
-from standardize_query import standardize_query
+from detecht_api.detecht_db_handling.search_table.standardize_query import standardize_query
 import time
 
-from models import Searches_Database
+from detecht_api.models import Searches_Database
 
 """
 Edward and Severn
 """
 
 # add new row into database. If there is no user_id, just set id as null.
-def add_row(query, id):
+def add(query, id):
     standardized_query = standardize_query(query)
     date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     search = Searches_Database()
-    search.add_row(id, date, query,standardized_query)
-    # obj = models.Searches_Database(user_id=id, search_date=date, search_query=query,standardized_search_query=standardized_query)
+    search.add_row(id, date, query,standardized_query,2)
+    # obj = Searches_Database(user_id=id, search_date=date, search_query=query,standardized_search_query=standardized_query,search_score=2)
     # obj.save()
 
 
@@ -24,6 +24,6 @@ if __name__ == '__main__':
              "the most perfect reasoning and observing machine"]
     for i in query:
         for j in range(1, 6):
-            add_row(i, j)
+            add(i, j)
             time.sleep(5)
     # print(models.Searches_Database.objects.all())
--- detecht_backend/detecht_api/models.py
+++ detecht_backend/detecht_api/models.py
@@ -95,7 +95,7 @@ Searches database
 Edward & Severn
 '''
 
-class Searches_Database(models.Manager):
+class Searches_Database(models.Model):
     user_id = models.IntegerField(null=True)  # not mandatory, can be NULL
     search_date = models.DateTimeField(auto_now=False, auto_now_add=True)  # when the search was done
     search_query = models.CharField(max_length=50)  # RAW search_query
@@ -104,6 +104,6 @@ class Searches_Database(models.Manager):
  MaxValueValidator(10)])
 
     def add_row(new_user_id, new_search_date, new_search_query, new_standardized_search_query, new_search_score):
-        new_search = Searches_Database(new_user_id, new_search_date, new_search_query, new_standardized_search_query, new_search_score)
+        new_search = Searches_Database(user_id=new_user_id, search_date=new_search_date, search_query=new_search_query, standardized_search_query=new_standardized_search_query, search_score=new_search_score)
         new_search.save()
 
