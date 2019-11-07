37fb23dc2454bd0920559a9d54b9f500812f940b
Oskar <oskhi827@student.liu.se>
2019-11-07 07:24:05 +0100

    models cleaned

--- detecht_backend/detecht_api/models.py
+++ detecht_backend/detecht_api/models.py
@@ -2,12 +2,6 @@ from django.db import models
 from django.core.files.storage import default_storage  # delete query
 
 
-class User(models.Model):
-    userID = models.IntegerField()  # could be removed since and id automaticly is generated. the id starts from 1 and is incrementing for each new tuple.
-    userName = models.TextField(max_length=50)
-    firstName = models.TextField(max_length=50)
-
-
 class Document(models.Model):
     title = models.CharField(max_length=200)
     file = models.FileField(upload_to='detecht_api/static/pdf', max_length=100, blank=True)
