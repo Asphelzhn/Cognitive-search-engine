5a4598cd7dbf4ab4237d6b463d9d7122d974a3d1
asphelzhn <568718851@qq.com>
2019-11-11 17:39:11 +0100

    add model

--- detecht_backend/detecht_api/detecht_db_handling/search_table/add_row.py
+++ detecht_backend/detecht_api/detecht_db_handling/search_table/add_row.py
@@ -1,4 +1,5 @@
 # from detecht_api import models
+from detecht_api import models
 from detecht_api.detecht_db_handling.search_table.standardize_query import standardize_query
 import time
 
@@ -8,12 +9,14 @@ from detecht_api.models import Searches_Database
 Edward and Severn
 """
 
-# add new row into database. If there is no user_id, just set id as null.
+# This is example of how to add new row into database.
+# If there is no user_id, just set id as null.
 def add(query, id):
     standardized_query = standardize_query(query)
     date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     search = Searches_Database()
-    search.add_row(id, date, query,standardized_query,2)
+    models.Searches_Database.objects.create(user_id=id,search_date=date,search_query=query,standardized_search_query=standardized_query,search_score=2)
+    #search.add_row(id, date, query,standardized_query)
     # obj = Searches_Database(user_id=id, search_date=date, search_query=query,standardized_search_query=standardized_query,search_score=2)
     # obj.save()
 
@@ -23,7 +26,7 @@ if __name__ == '__main__':
              "I have seldom heard him mention herunder any other name", "Irene Adler",
              "the most perfect reasoning and observing machine"]
     for i in query:
-        for j in range(1, 6):
+        for j in range(1, 5):
             add(i, j)
             time.sleep(5)
     # print(models.Searches_Database.objects.all())
