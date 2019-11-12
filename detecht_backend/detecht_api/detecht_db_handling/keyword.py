65a3124590b2c6794423d4030a0939558a5fd58a
Henrik Hörnlund <henho110@student.liu.se>
2019-11-12 13:34:37 +0100

    Created table for interacted documents and two ways of filling it Preview and download

--- detecht_backend/detecht_api/detecht_db_handling/keyword.py
+++ detecht_backend/detecht_api/detecht_db_handling/keyword.py
@@ -1,13 +1,15 @@
 from detecht_api.detecht_nlp.word_similarity import word_similarity
-from detecht_api.models import Keywords, Keyword_distance, Pdf_Name_Keyword_Weight
+from detecht_api.models import Keywords, Keyword_distance, Pdf_Name_Keyword_Weight, Interacted_documents
+from datetime import date
+
 
 # add keyword in db, if keyword alerady exists it is not added. If added true is returned. if it is in db False is returned
 def addKeyword(keyword):
     keyword, created = Keywords.objects.get_or_create(word=keyword)
 
-    if created: #True if keyword is added and does not exist in db
+    if created:  # True if keyword is added and does not exist in db
         keyword.save()
-        #run similarity for all Query
+        # run similarity for all Query
         allKeywords = Keywords.objects.exclude(word=keyword)
 
         for word in allKeywords:
@@ -18,7 +20,8 @@ def addKeyword(keyword):
 
 # add similarity for keyword.
 def KeywordSimilarity(keyword1, keyword2, keywordId2):
-    newDistance = Keyword_distance(id_1=Keywords.objects.get(word=keyword1).id, id_2=keywordId2, similarity=word_similarity(keyword1, keyword2))
+    newDistance = Keyword_distance(id_1=Keywords.objects.get(word=keyword1).id, id_2=keywordId2,
+                                   similarity=word_similarity(keyword1, keyword2))
     newDistance.save()
     return
 
@@ -28,3 +31,17 @@ def Add_Pdf_Name_Keyword_Weight(pdf, keyword, weight):
     new = Pdf_Name_Keyword_Weight(pdf_name=pdf, keyword=keyword, weight=weight)
     new.save()
     return
+
+
+
+# Henrik & Carl
+def Preview_Document(pdf_name1, userid1):
+    dateNow = date.today()
+    new = Interacted_documents(pdf_name=pdf_name1, date=dateNow, userid=userid1, down_prev="Preview")
+    new.save()
+
+
+def Download_Document(pdf_name1, userid1):
+    dateNow = date.today()
+    new = Interacted_documents(pdf_name=pdf_name1, date=dateNow, userid=userid1, down_prev="Download")
+    new.save()
