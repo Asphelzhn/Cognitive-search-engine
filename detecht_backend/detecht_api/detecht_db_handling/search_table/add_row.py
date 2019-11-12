acddee751bdaaa04c56d6accb234ceb7a68c4392
asphelzhn <568718851@qq.com>
2019-11-12 13:56:42 +0100

    fix get popular search in a time

--- detecht_backend/detecht_api/detecht_db_handling/search_table/add_row.py
+++ detecht_backend/detecht_api/detecht_db_handling/search_table/add_row.py
@@ -4,7 +4,7 @@ from random import Random
 from detecht_api import models
 from detecht_api.detecht_db_handling.search_table.standardize_query import standardize_query
 import time
-
+from datetime import datetime
 from detecht_api.models import Searches_Database
 
 """
@@ -17,7 +17,9 @@ def add_api(userid, query, date, score):
 
     standardized_query = standardize_query(query)
     if(date is None):
-        date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+
+        # date = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+        date = datetime.now()
 
     models.Searches_Database.objects.create(user_id=userid,search_date=date,search_query=query,standardized_search_query=standardized_query,search_score=score)
 
