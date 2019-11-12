822a5a1f965158c832b16036995b744c04e75a64
Samuel Persson <samuel.persson95@gmail.com>
2019-11-12 23:17:12 +0100

    API for one word autocomplete

--- detecht_backend/detecht_api/detecht_nlp/autocompleOneWord/autocomplete_one_word_api.py
+++ detecht_backend/detecht_api/detecht_nlp/autocompleOneWord/autocomplete_one_word_api.py
@@ -1,11 +1,33 @@
+# encoding: utf-8
+'''
+@author: Samuel
+@file: autocomplete_one_word_api.py
+@time: 2019/11/12 21:01
+@desc: This is using a trie to implement autocomplete for one word
+'''
 from detecht_api.detecht_nlp.autocompleOneWord import autocomplete_one_word
 import time
 import re
 from collections import Counter
 import sys
 
-
 def words(text): return re.findall(r'\w+', text.lower())
+
+class autocomplete_word():
+    def __init__(self,number_of_autocompletes):
+        # Creates hash
+        word_count = Counter(words(open("big.txt").read()))
+        # Makes it a dictionary
+        dict_for_trie = dict(word_count)
+        # Creates the datastructure and forms it accordning to the number of auto corrects
+        self.trie = autocomplete_one_word.Trie()
+        self.trie.form_trie(dict_for_trie, number_of_autocompletes)
+
+        # Returns autocompleted suggestions. Returns -1 if it's a full word without suggestions.
+        # Returns zero if there are no suggestions
+    def autocomplete_one_word_api(self,text):
+        suggestions = self.trie.find_word_suggestions(text)
+        return suggestions
 
 
 if __name__ == '__main__':
