8a1e244d14fe099941fd7e1d6b3774f21435106f
Carl Björklund <carlbjo@gmail.com>
2019-11-13 17:39:02 +0100

    Added page array to the json document. The json folder in static is required since it needs to store the output there

--- detecht_backend/detecht_api/detecht_converter/pdf_converter.py
+++ detecht_backend/detecht_api/detecht_converter/pdf_converter.py
@@ -1,58 +1,50 @@
-import PyPDF2
-import json
+from pdfminer.converter import TextConverter
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfpage import PDFPage
 import os
-import re
+import io
 
 def convert_pdf_to_json():
-
-    # get all PDFs  in the path
-    path = "detecht_api/static/pdf"
+    path = "detecht_api/static/pdf/"
     with os.scandir(path) as it:
         for entry in it:
             if entry.name.endswith(".pdf") and entry.is_file():
-                print("Currently scanning: " + entry.name)
-                file = entry.path
 
-                # read the current pdf
-                read_pdf = PyPDF2.PdfFileReader(file, strict=False)
-                all_pages = {}
 
-                # iterate all pages
+                fp = open(path + entry.name, 'rb')
+
+                # Iterate over the pages
+                pages = []
+                for page in PDFPage.get_pages(fp):
+                    rsrcmgr = PDFResourceManager()
+                    retstr = io.StringIO()
+                    device = TextConverter(rsrcmgr, retstr)
+                    interpreter = PDFPageInterpreter(rsrcmgr, device)
+
+
+                    interpreter.process_page(page)
+                    pagetext = retstr.getvalue()
+                    pagetext = pagetext[:-1]
+                    pages.append(pagetext)
+                    retstr.close()
+
+                #text = retstr.getvalue()
+
+                fp.close()
+                device.close()
+                retstr.close()
                 all_text = ""
-                for page in range(read_pdf.getNumPages()):
-                    data = read_pdf.getPage(page)
-                    page_text = data.extractText()
-                    modified_text = re.sub("\n", "", page_text)
-                    all_text = all_text + modified_text
-
-                all_pages["All_text"] = all_text
-
-                # creates a JSON file
+                for i in pages:
+                    all_text += i
+                # print the text to file
                 filename = entry.name
                 filename = filename[:-4]
                 filepath = 'detecht_api/static/json/' + filename + '.json'
-                with open(filepath,'w+') as outfile:
-                    json.dump(all_pages, outfile)
-
-def pdf_to_json(pdf_name):
-
-    # get all PDFs  in the path
-    path = "detecht_api/static/pdf/"
-    with open(path + pdf_name, 'rb') as file:
-
-        # read the current pdf
-        read_pdf = PyPDF2.PdfFileReader(file, strict=False)
-        all_pages = {}
-
-        # iterate all pages
-        all_text = ""
-        for page in range(read_pdf.getNumPages()):
-            data = read_pdf.getPage(page)
-            page_text = data.extractText()
-            modified_text = re.sub("\n", "", page_text)
-            all_text = all_text + modified_text
-
-        all_pages["All_text"] = all_text
-
-        return all_text
-
+                with open(filepath, 'w+', encoding="utf-8") as f:
+                    print("{\"All_text\": \"" + all_text + "\",", file=f)
+                    print("\"pages\":[", end="",file=f)
+                    for i in range(len(pages)-1):
+                        print(" \""+pages[i]+"\",", end="",file=f)
+                    print(" \""+pages[-1]+"\" ",end="",file=f)
+                    print("]",end="",file=f)
+                    print("}",end="",file=f)
