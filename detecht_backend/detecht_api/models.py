5b61ba65a77b364b871b3f9928362c7be2f7165a
Jakob <nilssonlindau@gmail.com>
2019-11-20 15:33:45 +0100

    Indent fix

--- detecht_backend/detecht_api/models.py
+++ detecht_backend/detecht_api/models.py
@@ -123,8 +123,7 @@ class Searches_Database(models.Model):
     search_date = models.DateTimeField(auto_now=False, auto_now_add=True)  # when the search was done
     search_query = models.CharField(max_length=50)  # RAW search_query
     standardized_search_query = models.CharField(max_length=50)  # standardization by using NLP spacy
-    search_score = models.IntegerField(null=True, validators=[MinValueValidator(1),
-                                                            MaxValueValidator(10)])
+    search_score = models.IntegerField(null=True, validators=[MinValueValidator(1), MaxValueValidator(10)])
 
     # add a new record into database
     def add_row(new_user_id, new_search_date, new_search_query, new_standardized_search_query, new_search_score):
