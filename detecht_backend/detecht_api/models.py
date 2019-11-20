c4160936d56bf56ef984239db3cac58bb84f99d5
Carl Björklund <carlbjo@gmail.com>
2019-11-20 10:58:38 +0100

    fixed codestandard

--- detecht_backend/detecht_api/models.py
+++ detecht_backend/detecht_api/models.py
@@ -11,6 +11,7 @@ class Document(models.Model):
     downloads = models.IntegerField(default=0)
     favorites = models.IntegerField(default=0)
 
+
     def __unicode__(self):
         return self.title
 
@@ -104,16 +105,17 @@ class Pdf_Similarities(models.Model):
 # Henrik & Carl
 
 class Interacted_documents(models.Model):
-
     down_prev = models.TextField(max_length=30)
     date = models.DateField()
     pdf_name = models.TextField(max_length=50)
     userid = models.TextField(max_length=20)
 
+
 '''
 Searches database
 Edward & Severn
 '''
+
 
 class Searches_Database(models.Model):
     user_id = models.IntegerField(null=True)  # not mandatory, can be NULL
@@ -121,7 +123,8 @@ class Searches_Database(models.Model):
     search_query = models.CharField(max_length=50)  # RAW search_query
     standardized_search_query = models.CharField(max_length=50)  # standardization by using NLP spacy
     search_score = models.IntegerField(null=True, validators=[MinValueValidator(1),
- MaxValueValidator(10)])
+    MaxValueValidator(10)])
+
 
     # add a new record into database
     def add_row(new_user_id, new_search_date, new_search_query, new_standardized_search_query, new_search_score):
@@ -136,4 +139,3 @@ class Searches_Database(models.Model):
 class User_Keyword(models.Model):
     userID = models.TextField(max_length=20)
     keyword = models.TextField(max_length=50)
-
