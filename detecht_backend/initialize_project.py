c2d564f29f9640019a3af52b72ec6c9f81f1ab87
Carl Björklund <carlbjo@gmail.com>
2019-11-20 10:34:16 +0100

    changed lines

--- detecht_backend/initialize_project.py
+++ detecht_backend/initialize_project.py
@@ -5,4 +5,3 @@ es = Elasticsearch()
 es.indices.delete(index='db', ignore=[400, 404])
 # ignore 400 cause by IndexAlreadyExistsException when creating an index
 es.indices.create(index='db', ignore=400)
-
