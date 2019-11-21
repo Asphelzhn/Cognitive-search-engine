00e5b2e37376847083b79b730aa75873ba838cf8
zhixiang Shi <zhish307@student.liu.se>
2019-11-21 14:12:30 +0100

    update

--- detecht_backend/detecht_api/detecht_nlp/autocompleteWords/n_gram.py
+++ detecht_backend/detecht_api/detecht_nlp/autocompleteWords/n_gram.py
@@ -2,14 +2,14 @@ import re
 import string
 import operator
 import time
-from detecht_api import models
+# from detecht_api import models
 # from detecht_backend.detecht_api import models
 '''
 By Severn
 '''
 def cleanText(input):
     input = re.sub('\n+', " ", input).lower()   # Match line breaks with spaces to replace spaces
-    input = re.sub('\[[0-9]*\]', "", input) # Eliminate reference tags like [1]
+    input = re.sub('\[[0-9]*\]', "", input) # Eliminate reference tags like [1]p
     input = re.sub(' +', " ", input)  # Replace consecutive spaces with one space
     # input = bytes(input)#.encode('utf-8') # Convert content to utf-8 format to eliminate escape characters
     # input = input.decode("ascii", "ignore")
@@ -41,6 +41,29 @@ def getNgrams(input, n):
     return output
 
 
+def get_result(query):
+    lines = open("result_1.txt").readlines()
+    suggestions = []
+    pattern = '.*'.join(query)
+    regex = re.compile(pattern)
+    i = 0
+    q = query.split()
+    length = len(q)
+    for line in lines:
+        match = regex.search(line)
+        s = line.split()
+        s_sub = []
+        w = ""
+        for m in range(length):
+            s_sub.append(s[m])
+        if w == query:
+            suggestions.append(re.sub('\n+', " ", line))
+            i += 1
+        if i > 7:
+            break
+    return suggestions
+
+
 if __name__ == '__main__':
     t1 = time.clock()
     content = open("big.txt").read()
@@ -53,10 +76,10 @@ if __name__ == '__main__':
             f.write('\n')
     '''
 
-    with open("result.txt","w") as f:
+    with open("result_1.txt","w") as f:
         for s in sortedNGrams:
             if s[1] > 1:
-                f.write(str(s[0])+":"+str(s[1]))
+                f.write(str(s[0]))
                 f.write('\n')
     t2 = time.clock()
     print(t2-t1)
