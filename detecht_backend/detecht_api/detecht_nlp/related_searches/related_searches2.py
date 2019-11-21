ce3252cf4c288674cdda41aba56a5f011468e871
Samuel Persson <samuel.persson95@gmail.com>
2019-11-21 18:16:42 +0100

    Finished with everything in suggestions, waiting on database to check if it works

--- detecht_backend/detecht_api/detecht_nlp/related_searches/related_searches2.py
+++ detecht_backend/detecht_api/detecht_nlp/related_searches/related_searches2.py
@@ -12,7 +12,7 @@ def delete_word(doc, index):
     return doc
 
 
-def generate_one_new_word(doc,number):
+def generate_one_new_word(doc,number, textsThatCantbeUsed):
     length = len(doc)
     if(length > maxGram):
         lowerIndex = length-maxGram
@@ -24,11 +24,12 @@ def generate_one_new_word(doc,number):
 
     "look over the data structure here"
     queryText = queryDatabase(span,1, number)
-
+    # Compare textList+queryText with text That cant be used
+    #Make it string
     return textList+queryText
 
 
-def generate_two_new_word(doc, number):
+def generate_two_new_word(doc, number, textsThatCantbeUsed):
     length = len(doc)
     print(length)
     thisGram=maxGram-1
@@ -41,29 +42,50 @@ def generate_two_new_word(doc, number):
     textList = doc[0:lowerIndex]
 
     queryText = queryDatabase(span, 2, number)
-
+    #Compare textList+queryText with text That cant be used
+    #Make ut string
     return textList+queryText
 
 
 
 def queryDatabase(text, numberOfExtraWords, numOfQueries):
-    i=len(text)
     list=[]
     list.append(text)
-    while(i>0):
+    i=0
+    while(i<len(text)):
+        i=i+1
         #append to list
+        # remove
+
         print("Query")
-        i=i-1
     #Retun list of texts with probabilities
     return list
+
+def compareWordList(queryReturn, wordsThatCantBeUsed):
+    for i in wordsThatCantBeUsed:
+        if(queryReturn==i):
+            return False
+    return True
+
 
 
 def generateRelatedSearches(text):
     list = string_to_list(text)
-    listDelete = delete_word(list.length)
-    oneWordDeletePlusOne = generate_one_new_word(listDelete,1)
-    oneWordDeletePlusTwo = generate_two_new_word(listDelete,1)
-    oneWordAdded = generate_one_new_word(list,1)
+    textsThatCantBeUsed=[]
+    textsThatCantBeUsed.append(list)
+
+    if(len(list)>1):
+        listDelete = delete_word(list.length)
+    else:
+        listDelete=list
+
+    oneWordDeletePlusOne = generate_one_new_word(listDelete,1,textsThatCantBeUsed)
+    textsThatCantBeUsed.append(oneWordDeletePlusOne)
+
+    oneWordDeletePlusTwo = generate_two_new_word(listDelete,1,textsThatCantBeUsed)
+    oneWordDeletePlusOne.append(oneWordDeletePlusTwo)
+
+    oneWordAdded = generate_one_new_word(list,1,textsThatCantBeUsed)
     returnList= oneWordDeletePlusOne+oneWordDeletePlusTwo+oneWordAdded
     return returnList
 
@@ -75,6 +97,10 @@ def string_to_list(text):
         list.append(word.text)
     return list
 
+def list_to_string(list):
+    str1 = ''.join(list)
+    return str1
+
 def main():
     text="hej jag heter"
     text=string_to_list(text)
@@ -82,7 +108,7 @@ def main():
     doc =delete_word(text,2)
     print(doc)
     t3=time.time()
-    print(generate_two_new_word(["how", "are", "you"],1))
+    print(generate_two_new_word(["how", "are", "you"],2,["how"]))
     t4=time.time()
 
 
