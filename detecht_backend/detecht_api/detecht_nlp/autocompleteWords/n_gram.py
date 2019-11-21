56c702b1b3cf0f1e9b2d3cd4df15828745eb1cb3
zhixiang Shi <zhish307@student.liu.se>
2019-11-21 15:25:53 +0100

    update

--- detecht_backend/detecht_api/detecht_nlp/autocompleteWords/n_gram.py
+++ detecht_backend/detecht_api/detecht_nlp/autocompleteWords/n_gram.py
@@ -56,6 +56,7 @@ def get_result(query):
         w = ""
         for m in range(length):
             s_sub.append(s[m])
+        w.join(s_sub)
         if w == query:
             suggestions.append(re.sub('\n+', " ", line))
             i += 1
