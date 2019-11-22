128aaa51a52f0019d3ae9e99c774225381499ace
zhixiang Shi <zhish307@student.liu.se>
2019-11-22 22:30:10 +0100

    function: upload data to the database

---
+++ detecht_backend/detecht_api/detecht_nlp/autocompleteWords/upload_n_gram.py
@@ -0,0 +1,32 @@
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'detecht_backend.settings'
+import django
+
+django.setup()
+import re
+import string
+import operator
+import time
+from detecht_api.models import Search_Autocomplete
+from detecht_api.detecht_nlp.autocompleteWords import n_gram
+
+
+def upload():
+    t1 = time.clock()
+    content = open("big.txt").read()
+    n_grams = n_gram.getNgrams(content, 6)
+    sortedNGrams = sorted(n_grams.items(), key=operator.itemgetter(1), reverse=True)  # =True descending sort
+    t2 = time.clock()
+    print("Data processing time: " + str(t2-t1))
+    # delete old data in  the database
+    Search_Autocomplete.objects.all().delete()
+    # write data in to database
+    work_list = []
+    for s in sortedNGrams:
+        if s[1] > 1:
+            work_list.append(Search_Autocomplete(n_gram=str(s[0]), frequency=int(s[1])))
+    Search_Autocomplete.objects.bulk_create(work_list)
+    t3 = time.clock()
+    print("Data upload time: " + str(t3-t2))
+    print("uploaded")
