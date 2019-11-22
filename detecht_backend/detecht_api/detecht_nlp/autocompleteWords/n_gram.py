1c2fc626ab00577281e850f1aaf38516305be414
zhixiang Shi <zhish307@student.liu.se>
2019-11-22 22:30:28 +0100

    update

--- detecht_backend/detecht_api/detecht_nlp/autocompleteWords/n_gram.py
+++ detecht_backend/detecht_api/detecht_nlp/autocompleteWords/n_gram.py
@@ -51,46 +51,4 @@ def getNgrams(input, n):
     return output
 
 
-def upload():
-    content = open("big.txt").read()
-    ngrams = getNgrams(content, 5)
-    sortedNGrams = sorted(ngrams.items(), key=operator.itemgetter(1), reverse=True)
-    for s in sortedNGrams:
-        if s[1] > 1:
-            Search_Autocomplete.add_row(str(s[0]), int(s[1]))
-    print("uploaded!")
 
-
-if __name__ == '__main__':
-    t1 = time.clock()
-    content = open("big.txt").read()
-    ngrams = getNgrams(content, 6)
-    sortedNGrams = sorted(ngrams.items(), key=operator.itemgetter(1), reverse=True)  # =True descending sort
-    '''
-    with open("result.txt","w") as f:
-        for key, value in ngrams.items():
-            f.write(key+":"+str(value))
-            f.write('\n')
-    '''
-    '''
-    with open("result_1.txt","w") as f:
-        for s in sortedNGrams:
-            if s[1] > 1:
-                f.write(str(s[0]))
-                f.write('\n')
-    t2 = time.clock()
-    print(t2-t1)
-    '''
-    t2 = time.clock()
-    print(t2-t1)
-    # write data in to database
-    Search_Autocomplete.objects.all().delete()
-    worklist = []
-    for s in sortedNGrams:
-        if s[1] > 1:
-            worklist.append(Search_Autocomplete(n_gram=str(s[0]), frequency=int(s[1])))
-            #Search_Autocomplete.objects.create(n_gram=str(s[0]), frequency=int(s[1]))
-    Search_Autocomplete.objects.bulk_create(worklist)
-    t3 = time.clock()
-    print(t3-t2)
-    print("uploaded")
