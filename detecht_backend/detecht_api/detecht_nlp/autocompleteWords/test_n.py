f7e5cdc7157f6ac6fdd7f476cf8e551ba3558399
zhixiang Shi <zhish307@student.liu.se>
2019-11-22 22:15:43 +0100

    upload data to the database

--- detecht_backend/detecht_api/detecht_nlp/autocompleteWords/test_n.py
+++
@@ -1,8 +0,0 @@
-from detecht_api.detecht_nlp.autocompleteWords import n_gram
-import re
-
-def test():
-    query = "the"
-    #output = re.sub('\n+', " ", n_gram.get_result(query))
-
-    print(n_gram.get_result(query))
