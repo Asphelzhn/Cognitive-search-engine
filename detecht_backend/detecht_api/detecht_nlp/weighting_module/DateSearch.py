86560a810ff592ddcbf96d4b1a045973a088b852
asphelzhn <568718851@qq.com>
2019-11-24 23:40:11 +0100

    create function for filter date

--- detecht_backend/detecht_api/detecht_nlp/weighting_module/DateSearch.py
+++ detecht_backend/detecht_api/detecht_nlp/weighting_module/DateSearch.py
@@ -5,4 +5,18 @@
 @time: 2019/11/23 10:01
 @desc: This is the function for searching document creation date
 '''
+from detecht_api.detecht_nlp.weighting_module.WeightingModule import WeightingModule
 
+
+def filter_by_date(start_date,end_date,elastic_search_results):
+
+    WeightingModule.calculate_score_after_weight(elastic_search_results, search_query)
+
+# This is the example of using Weighting Module to filter the document in a specific date
+if __name__ == '__main__':
+    elastic_search_results = ['Project management', 'python is amazing', 'programming book']
+    query = "I like computer"
+
+    sorted_list = WeightingModule.calculate_score_after_weight(elastic_search_results, query)
+
+    print(sorted_list)
