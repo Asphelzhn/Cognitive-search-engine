89a1f7356e34d0510e233b3922ed291563c8c8d8
Nicklas Kull <nicku318@student.liu.se>
2019-11-24 19:41:00 +0100

    Added localhost to allowed hosts

--- detecht_backend/detecht_backend/settings.py
+++ detecht_backend/detecht_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ft*c^gqzv==7c17a_18ltje&^0vf)0=l&-t0wp-8x_6y4*g6jz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['company-1-1-tddc88-2019.edu.liu.se']
+ALLOWED_HOSTS = ['company-1-1-tddc88-2019.edu.liu.se', 'localhost']
 
 
 # Application definition
