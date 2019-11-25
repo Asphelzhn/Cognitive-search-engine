aba3db56c52d14df997c9c7e99cec71c2c8f2f17
Samuel Persson <samuel.persson95@gmail.com>
2019-11-25 17:32:35 +0100

    Finished related searches. Ready for testing

--- detecht_backend/detecht_api/detecht_nlp/related_searches/related_searches2.py
+++ detecht_backend/detecht_api/detecht_nlp/related_searches/related_searches2.py
@@ -1,5 +1,13 @@
 import spacy
 import time
+import os
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'detecht_backend.settings'
+import django
+
+django.setup()
+from detecht_api.models import Search_Autocomplete
+
 from spacy.attrs import LOWER, POS, ENT_TYPE, IS_ALPHA
 from spacy.tokens import Doc
 import numpy
@@ -8,58 +16,116 @@ nlp = spacy.load("en_core_web_sm")
 maxGram = 4
 
 def delete_word(doc, index):
-    del doc[index]
-    return doc
+    newList=doc.copy()
+    del newList[index]
+    return newList
 
 
 def generate_one_new_word(doc,number, textsThatCantbeUsed):
-    length = len(doc)
-    if(length > maxGram):
-        lowerIndex = length-maxGram
-    else:
-        lowerIndex = 0
-
-    span = doc[lowerIndex:length]
-    textList = doc[0:lowerIndex]
-
-    "look over the data structure here"
-    queryText = queryDatabase(span,1, number)
-    # Compare textList+queryText with text That cant be used
-    #Make it string
-    return textList+queryText
+    queryText = queryDatabase(doc,1, number, textsThatCantbeUsed)
+    return queryText
 
 
 def generate_two_new_word(doc, number, textsThatCantbeUsed):
-    length = len(doc)
-    print(length)
-    thisGram=maxGram-1
-    if (length > thisGram):
-        lowerIndex = length - thisGram
+    queryText = queryDatabase(doc, 2, number, textsThatCantbeUsed)
+    return queryText
+
+
+# numOfExtraWords < 3
+def queryDatabase(querySplit, numberOfExtraWords, numOfQueries, textsThatCantBeUsed):
+    if(numberOfExtraWords>3):
+        return 0
+
+    queryBeginning = []
+    lengthOfQuery=len(querySplit)
+
+    if(lengthOfQuery>4):
+        index=lengthOfQuery-4
     else:
-        lowerIndex = 0
+        index=0
+    tempQuery=list_to_string(querySplit)
 
-    span = doc[lowerIndex:length]
-    textList = doc[0:lowerIndex]
+    tempQuery = list_to_string(querySplit[index:lengthOfQuery])
+    result = Search_Autocomplete.objects.filter(n_gram__istartswith=tempQuery)
+    resultCount=result.count()
 
-    queryText = queryDatabase(span, 2, number)
-    #Compare textList+queryText with text That cant be used
-    #Make ut string
-    return textList+queryText
+    while(resultCount<numOfQueries):
+        tempQuery= list_to_string(querySplit[index:lengthOfQuery])
+        #print(tempQuery)
+        result = Search_Autocomplete.objects.filter(n_gram__istartswith=tempQuery)
+        index=index+1
+        resultCount = result.count()
+        if(index>lengthOfQuery):
+            return 0
+
+    if(index>0):
+        queryBeginning = querySplit[0:index-1]
 
 
+    gram_list = []
+    for r in result:
+        gram_list.append(r.n_gram)
+    n2gram = []
+    n3gram = []
+    n4gram = []
+    n5gram = []
+    for g in gram_list:
+        if g not in textsThatCantBeUsed:
+            if len(g.split()) == 2:
+                n2gram.append(g)
+            elif len(g.split()) == 3:
+                n3gram.append(g)
+            elif len(g.split()) == 3:
+                n3gram.append(g)
+            elif len(g.split()) == 4:
+                n4gram.append(g)
+            elif len(g.split()) == 5:
+                n5gram.append(g)
 
-def queryDatabase(text, numberOfExtraWords, numOfQueries):
-    list=[]
-    list.append(text)
-    i=0
-    while(i<len(text)):
-        i=i+1
-        #append to list
-        # remove
+    #print(n2gram)
+    #print(n3gram)
+    #print(n4gram)
+    #print(n5gram)
 
-        print("Query")
-    #Retun list of texts with probabilities
-    return list
+    ##ERROR IN NUMBER OF EXTRAWORDS
+    tempQueryLen = len(tempQuery.split())
+    result_list = []
+    if tempQueryLen == 1:
+        if numberOfExtraWords==2:
+            result_list = n3gram[:numOfQueries]
+        elif numberOfExtraWords==1:
+            result_list = n2gram[:numOfQueries]
+        else:
+            return 0
+    elif tempQueryLen == 2:
+        if numberOfExtraWords==2:
+            result_list = n4gram[:numOfQueries]
+        elif numberOfExtraWords==1:
+            result_list = n3gram[:numOfQueries]
+        else:
+            return 0
+    elif tempQueryLen == 3:
+        if numberOfExtraWords==2:
+            result_list = n5gram[:numOfQueries]
+        elif numberOfExtraWords==1:
+            result_list = n4gram[:numOfQueries]
+        else:
+            return 0
+    elif tempQueryLen == 4:
+        if numberOfExtraWords == 2:
+            return 0
+        elif numberOfExtraWords == 1:
+            result_list = n5gram[:numOfQueries]
+        else:
+            return 0
+
+    firstPart= list_to_string(queryBeginning)
+
+    if(len(firstPart)>0):
+        firstPart=firstPart+" "
+
+    return [firstPart + s for s in result_list]
+
 
 def compareWordList(queryReturn, wordsThatCantBeUsed):
     for i in wordsThatCantBeUsed:
@@ -70,23 +136,41 @@ def compareWordList(queryReturn, wordsThatCantBeUsed):
 
 
 def generateRelatedSearches(text):
+    if (text==""):
+        return 0
+
     list = string_to_list(text)
     textsThatCantBeUsed=[]
-    textsThatCantBeUsed.append(list)
+    textsThatCantBeUsed.append(text)
 
     if(len(list)>1):
-        listDelete = delete_word(list.length)
+
+        listDelete = delete_word(list,len(list)-1)
     else:
         listDelete=list
+    resultsToBeGenerated=1
+    oneWordDeletePlusOne = generate_one_new_word(listDelete,resultsToBeGenerated,textsThatCantBeUsed)
 
-    oneWordDeletePlusOne = generate_one_new_word(listDelete,1,textsThatCantBeUsed)
-    textsThatCantBeUsed.append(oneWordDeletePlusOne)
+    if oneWordDeletePlusOne!=0:
+        textsThatCantBeUsed=textsThatCantBeUsed+oneWordDeletePlusOne
+    else:
+        resultsToBeGenerated = resultsToBeGenerated + 1
 
-    oneWordDeletePlusTwo = generate_two_new_word(listDelete,1,textsThatCantBeUsed)
-    oneWordDeletePlusOne.append(oneWordDeletePlusTwo)
+    oneWordDeletePlusTwo = generate_two_new_word(listDelete,resultsToBeGenerated,textsThatCantBeUsed)
+    if oneWordDeletePlusTwo != 0:
+        textsThatCantBeUsed = textsThatCantBeUsed+oneWordDeletePlusTwo
+        if len(oneWordDeletePlusTwo)==resultsToBeGenerated:
+            resultsToBeGenerated=1
+        else:
+            resultsToBeGenerated=resultsToBeGenerated-len(oneWordDeletePlusTwo)
+    else:
+        resultsToBeGenerated=resultsToBeGenerated+1
 
-    oneWordAdded = generate_one_new_word(list,1,textsThatCantBeUsed)
-    returnList= oneWordDeletePlusOne+oneWordDeletePlusTwo+oneWordAdded
+    oneWordAdded = generate_one_new_word(list,resultsToBeGenerated,textsThatCantBeUsed)
+    if oneWordAdded !=0:
+        returnList= oneWordDeletePlusOne+oneWordDeletePlusTwo+oneWordAdded
+    else:
+        returnList= oneWordDeletePlusOne+oneWordDeletePlusTwo
     return returnList
 
 
@@ -98,29 +182,20 @@ def string_to_list(text):
     return list
 
 def list_to_string(list):
-    str1 = ''.join(list)
+    str1 = ' '.join(list)
     return str1
 
 def main():
-    text="hej jag heter"
-    text=string_to_list(text)
-    print(text)
-    doc =delete_word(text,2)
-    print(doc)
-    t3=time.time()
-    print(generate_two_new_word(["how", "are", "you"],2,["how"]))
+
+
+    #print(generate_two_new_word(["how", "are", "you"],2,["how"]))
+    str=input('Enter your search: ')
+    t3 = time.time()
+    print(generateRelatedSearches(str))
+    #print(generate_two_new_word(["what", "is"],10,["how"]))
+    #print(list_to_string(["hej", "hur"]))
     t4=time.time()
-
-
-    # doc =nlp("are you and somebody")
-    # print(generate_one_new_word(doc,1))
-    # doc = nlp("are you and")
-    # print(generate_one_new_word(doc,1))
-    # doc = nlp("are you")
-    # print(generate_one_new_word(doc,1))
-    # doc = nlp("are")
-    # print(generate_one_new_word(doc,1))
-    # print(type(generate_one_new_word(doc,1)))
+    print(t4-t3)
 
 
 
