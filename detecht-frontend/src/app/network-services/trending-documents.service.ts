b14b22df39c00f04d2e6ff633c2d8fdaeb8c2140
Oskar <oskhi827@student.liu.se>
2019-11-26 14:45:48 +0100

    Trending doc done

--- detecht-frontend/src/app/network-services/trending-documents.service.ts
+++ detecht-frontend/src/app/network-services/trending-documents.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import {NetworkService} from './network.service';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {NetworkTrendingDocumentsResponse} from './network-data-types';
+import {environment} from '../../environments/environment';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrendingDocumentsService {
 
-  constructor() { }
+  constructor(private networkService: NetworkService, private http: HttpClient) { }
+
+  trendingDocuments(size: number): Observable<NetworkTrendingDocumentsResponse> {
+    return this.http.post<NetworkTrendingDocumentsResponse>(environment.apiUrl + 'trendingdocuments/', {size}, {
+      withCredentials: true,
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    }).pipe(catchError(this.networkService.handleError));
+  }
 }
