3f43b7db7e3c9664d887071bd0dc82f364420c8c
Nicklas Kull <nicku318@student.liu.se>
2019-12-02 12:20:10 +0100

    Environment settings

--- detecht-frontend/src/environments/environment.prod.ts
+++ detecht-frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000/api/',
-  plainUrl: 'http://localhost:8000/',
-  pdfUrl: 'company-1-1-tddc88-2019.edu.liu.se/static/pdf/',
-  staticUrl: 'http://localhost:8000/static/'
+  apiUrl: 'api/',
+  plainUrl: '',
+  pdfUrl: 'static/pdf/',
+  staticUrl: 'static/'
 };
