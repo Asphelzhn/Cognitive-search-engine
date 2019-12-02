476405eed8f6a2296c9c762ae31299f78704d71e
Nicklas Kull <nicku318@student.liu.se>
2019-12-02 14:06:48 +0100

    Updated environment variables again

--- detecht-frontend/src/environments/environment.prod.ts
+++ detecht-frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   apiUrl: 'api/',
   plainUrl: '',
-  pdfUrl: 'static/pdf/',
+  pdfUrl: 'http://company-1-1-tddc88-2019.edu.liu.se/static/pdf/',
   staticUrl: 'static/'
 };
